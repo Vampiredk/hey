235ed36b4171f344953c5f0dfe29a17c6f4d9b8b
Vampiredk <simonbreit2@gmail.com>
2017-01-04 10:55:13 +0100

    Hey
    I i did know, i would had said it

---
+++ JS Opgave 1.5C.js
@@ -0,0 +1,24 @@
+ // Global værdier
+
+var tal1;
+var tal2;
+var tal3;
+
+function konventer(){
+	tal1=parseFloat(document.test.tal_1.value);
+	tal2=parseFloat(document.test.tal_2.value);
+	tal3=parseFloat(document.test.tal_3.value);
+}
+
+function sum1(){
+	konventer();
+	var resultat =tal2 * tal3;
+	document.test.kommentar1.value=resultat;
+}
+
+function sum2(){
+	konventer();	
+	var resultat =tal1 * tal2 * tal3;
+	document.test.kommentar2.value=resultat;
+}
+
